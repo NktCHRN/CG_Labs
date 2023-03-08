dade6d43e5a877b14007926fcbb3427a400ac09d
Hlebushkek <volsoor@gmail.com>
2023-03-08 17:27:24 +0200

    Disk & Plane intersection
    Close #3 and #4

--- Lab1/Lab1.RayTracer/SceneObjects/Disk.cs
+++ Lab1/Lab1.RayTracer/SceneObjects/Disk.cs
@@ -7,6 +7,22 @@ class Disk : BaseSceneObject
     public Disk(Vector3f position, Vector3f rotation) : this(position, rotation, 1) {}
     public Disk() : this(new Vector3f(0), new Vector3f(0)) {}
     
-    public override bool IsIntersectedBy(in Ray ray) => false;
+    public override bool IsIntersectedBy(in Ray ray)
+    {
+        Vector3f normal = Direction;
+
+        float denom = normal.DotProduct(Position);
+
+        float v = normal.DotProduct(ray.Direction);
+        if (MathF.Abs(v) < 0.001)
+            return false;
+
+        float t = (denom - normal.DotProduct(ray.StartPoint)) / normal.DotProduct(ray.Direction);
+
+        Vector3f newRay = ray.Direction * t;
+        Vector3f contact = ray.StartPoint + newRay;
+
+        return (contact - Position).Length() < _radius;
+    }
     public override void ObjectWasPlaced() {}
 }
--- Lab1/Lab1.RayTracer/SceneObjects/Plane.cs
+++ Lab1/Lab1.RayTracer/SceneObjects/Plane.cs
@@ -7,6 +7,24 @@ class Plane : BaseSceneObject
     public Plane(Vector3f position, Vector3f rotation) : this(position, rotation, new Vector3f(0)) {}
     public Plane() : this(new Vector3f(0), new Vector3f(0)) {}
     
-    public override bool IsIntersectedBy(in Ray ray) => false;
+    public override bool IsIntersectedBy(in Ray ray)
+    {
+        Vector3f normal = Direction;
+
+        float denom = normal.DotProduct(Position);
+
+        float v = normal.DotProduct(ray.Direction);
+        if (MathF.Abs(v) < 0.001)
+            return false;
+
+        float t = (denom - normal.DotProduct(ray.StartPoint)) / normal.DotProduct(ray.Direction);
+
+        Vector3f newRay = ray.Direction * t;
+        Vector3f contact = ray.StartPoint + newRay;
+
+        Vector3f contactVec = contact - Position;
+
+        return Math.Abs(contactVec.X) <= _size.X && Math.Abs(contactVec.Y) <= _size.Y;
+    }
     public override void ObjectWasPlaced() {}
 }
