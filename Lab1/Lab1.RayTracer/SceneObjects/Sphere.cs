a5f86fc67f5d38f47482b8f4247011ad02f5831b
idontsmileinpublic <maystrenko.sonya@gmail.com>
2023-03-12 17:33:45 +0100

    Sphere

---
+++ Lab1/Lab1.RayTracer/SceneObjects/Sphere.cs
@@ -0,0 +1,31 @@
+﻿namespace Lab1.RayTracer.SceneObjects;
+
+public class Sphere : BaseSceneObject
+{
+    private readonly float _radius;
+
+    public Sphere(Vector3f position, Vector3f rotation, float radius) : base(position, rotation) => _radius = radius;
+    public Sphere(Vector3f position, Vector3f rotation) : this(position, rotation, 1) { }
+    public Sphere() : this(new Vector3f(0), new Vector3f(0)) { }
+
+    public override bool IsIntersectedBy(in Ray ray)
+    {
+        var origin = ray.StartPoint;
+        var center = Position;
+        var k = origin - center;
+
+        var directionSquared = ray.Direction.DotProduct(ray.Direction);
+        var radiusSquared = _radius * _radius;
+        var kSquared = k.DotProduct(k);
+
+        var a = directionSquared;
+        var b = 2 * ray.Direction.DotProduct(k);
+        var c = kSquared - radiusSquared;
+
+        var discriminantSquared = b * b - 4 * a * c;
+
+        return discriminantSquared >= 0;
+    }
+
+    public override void ObjectWasPlaced() { }
+}
