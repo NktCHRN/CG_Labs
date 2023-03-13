3e83b50f2030c128c98820e80ec9dcf5ddb00902
Chernikov Nikita <14nik20@gmail.com>
2023-03-13 09:51:06 +0200

    Refactored the vertical FOV property

--- Lab1/Lab1.RayTracer/SceneObjects/Camera.cs
+++ Lab1/Lab1.RayTracer/SceneObjects/Camera.cs
@@ -1,12 +1,24 @@
 namespace Lab1.RayTracer;
 public class Camera : BaseSceneObject
 {
-    public float VerticalFieldOfView { get; } = 45;
+    private float _verticalFieldOfView = 45;
+
+    public float VerticalFieldOfView
+    {
+        get
+        {
+            return _verticalFieldOfView;
+        }
+        set
+        {
+            if (value <= 0 || value >= 180)
+                throw new ArgumentOutOfRangeException(nameof(value), "Vertical FOV must be in between 0 and 180 deg.");
+            _verticalFieldOfView = value;
+        }
+    }
 
     public Camera(Vector3F position, Vector3F rotation, float verticalFieldOfView = 45) : base(position, rotation) 
     {
-        if (VerticalFieldOfView <= 0 || VerticalFieldOfView >= 180)
-            throw new ArgumentOutOfRangeException(nameof(verticalFieldOfView), "Vertical FOV must be in between 0 and 180 deg.");
         VerticalFieldOfView = verticalFieldOfView;
     }
     public Camera() : base() {}
