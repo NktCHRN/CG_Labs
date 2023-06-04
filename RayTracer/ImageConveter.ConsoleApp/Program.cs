2b1bc5ff2a9ae4d63013172536301ae0f3008da3
Chernikov Nikita <14nik20@gmail.com>
2023-06-04 20:03:41 +0300

    Removed unnecessary usings and references

--- RayTracer/ImageConveter.ConsoleApp/Program.cs
+++ RayTracer/ImageConveter.ConsoleApp/Program.cs
@@ -1,9 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using ImageConverter.Core;
-using ImageConverter.Reader.BMP;
-using ImageConverter.Reader.PPM;
-using ImageConverter.Writer.BMP;
-using ImageConverter.Writer.PPM;
+﻿using ImageConverter.Core;
 
 Console.WriteLine("Hello, World!");
 var pluginManager = new PluginManager();
