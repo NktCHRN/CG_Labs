888fa7c7f4bd41b5280ed4c1a90cb1c6db1b071e
Chernikov Nikita <14nik20@gmail.com>
2023-06-04 12:28:01 +0300

    Added basic readers/writers

---
+++ RayTracer/ImageConverter.Writer.BMP/BmpWriter.cs
@@ -0,0 +1,14 @@
+﻿using ImageConverter.Common;
+using System.Drawing;
+
+namespace ImageConverter.Writer.BMP;
+public sealed class BmpWriter : IImageWriter
+{
+    public string ImageFormat => throw new NotImplementedException();
+
+    public void Write(string fileName, Color[,] bitMap)
+    {
+        throw new NotImplementedException();
+    }
+}
+
