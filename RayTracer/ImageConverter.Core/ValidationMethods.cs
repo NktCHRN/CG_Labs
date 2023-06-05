5e3fa113465bc129a8318d4ed4eb8fad1e6e839c
Chernikov Nikita <14nik20@gmail.com>
2023-06-05 09:26:03 +0300

    Added HelperMethods and ValidateMethods

---
+++ RayTracer/ImageConverter.Core/ValidationMethods.cs
@@ -0,0 +1,23 @@
+﻿namespace ImageConverter.Core;
+public static class ValidationMethods
+{
+    public static void ValidateFileName(string fileName)
+    {
+        if (string.IsNullOrWhiteSpace(fileName))
+        {
+            throw new ArgumentNullException(nameof(fileName), "Provided file name was null or whitespace");
+        }
+    }
+
+    public static void ValidateFileExtension(string fileExtension)
+    {
+        if (string.IsNullOrWhiteSpace(fileExtension))
+        {
+            throw new ArgumentNullException(nameof(fileExtension), "Provided file extension was null or whitespace");
+        }
+        else if (!fileExtension.Any(c => char.IsLetterOrDigit(c)))
+        {
+            throw new ArgumentException("Provided file extension does not contain any characters or digits");
+        }
+    }
+}
